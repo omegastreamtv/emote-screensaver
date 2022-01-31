b9fba6aec6116878f9b9fcc4a4d5c050cb5fa64b
Mahcks <maxnatural13@gmail.com>
2022-01-30 21:41:22 -0600

    Update index.js
    Cleaned up old emote array that's not used anymore

--- index.js
+++ index.js
@@ -1,60 +1,3 @@
-const emotes = [
-    "https://cdn.7tv.app/emote/61c4cf18b3cc6a0abefc8c97/3x",
-    "https://static-cdn.jtvnw.net/emoticons/v2/300272604/default/dark/3.0",
-    "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_53463131dd7c46dfb74ef209e25d2ab6/default/dark/3.0",
-    "https://static-cdn.jtvnw.net/emoticons/v2/307047539/default/dark/3.0",
-    "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_38b199e51ccb4d57aeca1ea21354aa8a/default/dark/3.0",
-    "https://static-cdn.jtvnw.net/emoticons/v2/1086325/default/dark/3.0",
-    "https://static-cdn.jtvnw.net/emoticons/v2/301622790/default/dark/3.0",
-    "https://cdn.7tv.app/emote/60bd31df45d2e7506fe2a018/3x",
-    "https://cdn.7tv.app/emote/618db963d34608492cc2d6fb/3x",
-    "https://cdn.frankerfacez.com/emote/128054/4",
-    "https://cdn.betterttv.net/emote/6194b95354f3344f8805fd7b/3x",
-    "https://cdn.betterttv.net/emote/60d211128ed8b373e4217d8b/3x",
-    "https://cdn.betterttv.net/emote/61060daa2d1eba5400d2bbad/3x",
-    "https://cdn.betterttv.net/emote/60d61d228ed8b373e42197f4/3x",
-    "https://cdn.betterttv.net/emote/6019cb5282cf6865d553b0d3/3x",
-    "https://cdn.betterttv.net/emote/5e87b595acae25096140ca84/3x",
-    "https://cdn.betterttv.net/emote/5fd707c6a926f43485ce5e82/3x",
-    "https://cdn.betterttv.net/emote/5a8314b61686393232d31027/3x",
-    "https://cdn.betterttv.net/emote/618354c71f8ff7628e6c5b03/3x",
-    "https://cdn.betterttv.net/emote/61cefa60c8cc7f36d52b217c/3x",
-    "https://cdn.betterttv.net/emote/61807e741f8ff7628e6bfbe0/3x",
-    "https://cdn.7tv.app/emote/60e4fd747fa83e524442eff8/3x",
-    "https://cdn.7tv.app/emote/619fb59915b3ff4a5bb7a90a/3x",
-    "https://cdn.betterttv.net/emote/5fc19cede2900c6f07dfc836/3x",
-    "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_af715ca82e9145719203afa76710b08c/default/dark/3.0",
-    "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_21f1b9cecdd44355961d343f6e88789d/default/dark/3.0",
-    "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_fb1673c5061b4286a802b8b549c91196/default/dark/3.0",
-    "https://cdn.7tv.app/emote/61d4bba4752f555bcde9acbb/3x",
-    "https://cdn.7tv.app/emote/60ae7b00c4b981d367cd0123/3x",
-    "https://cdn.betterttv.net/emote/5ed5943f924aa35e32a6963c/3x",
-    "https://cdn.betterttv.net/emote/604104b7306b602acc596df6/3x",
-    "https://cdn.betterttv.net/emote/610f7d9e76ea4e2b9f760126/3x",
-    "https://cdn.betterttv.net/emote/5f076ee2a2ac620530366f7a/3x",
-    "https://cdn.betterttv.net/emote/5e500538e383e37d5d9dd3ec/3x",
-    "https://cdn.frankerfacez.com/emote/262149/4",
-    "https://cdn.frankerfacez.com/emote/229486/4",
-    "https://cdn.7tv.app/emote/615636736251d7e000db35ec/3x",
-    "https://cdn.7tv.app/emote/60caf39c3591d98c4059c5c0/3x",
-    "https://cdn.betterttv.net/emote/61cd58a7c8cc7f36d52b01a9/3x",
-    "https://cdn.betterttv.net/emote/609431bc39b5010444d0cbdc/3x",
-    "https://cdn.betterttv.net/emote/603451b77c74605395f3295d/3x",
-    "https://cdn.betterttv.net/emote/5ffd66da465444316bf608ab/3x",
-    "https://cdn.betterttv.net/emote/608c699d39b5010444d094ef/3x",
-    "https://cdn.betterttv.net/emote/618019f01f8ff7628e6beb1a/3x",
-    "https://cdn.betterttv.net/emote/61cef923c8cc7f36d52b2168/3x",
-    "https://cdn.betterttv.net/emote/5fa01b3740eb9502e22399bb/3x",
-    "https://cdn.betterttv.net/emote/5f99d6d3710f8302f0c96316/3x",
-    "https://cdn.betterttv.net/emote/603fcd96306b602acc59651e/3x",
-    "https://cdn.betterttv.net/emote/60b5121ff8b3f62601c363a2/3x",
-    "https://cdn.betterttv.net/emote/5f9b383540eb9502e2234d56/3x",
-    "https://cdn.betterttv.net/emote/60ff190b2d1eba5400d1b4b6/3x",
-    "https://cdn.betterttv.net/emote/617fba2c1f8ff7628e6be0da/3x",
-    "https://cdn.betterttv.net/emote/617fba081f8ff7628e6be0cb/3x"
-
-];
-
 let emoteData = [];
 (async () => {
     let bttvFetch = await fetch(`https://api.betterttv.net/3/cached/users/twitch/38746172`);
@@ -97,8 +40,7 @@ let emoteData = [];
             emoteData.push({ name: sevenTVData[i]["name"], link: `https://cdn.7tv.app/emote/${sevenTVData[i]["id"]}/4x` });
         }
     }
-
-    // {name: "", link: ""}
+    
     let subemotes = await fetch("https://api.retpaladinbot.com/emotes/esfandtv");
     let subData = await subemotes.json();
     if (subData) {
