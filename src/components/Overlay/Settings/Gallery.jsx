0c1fc1b42e392f319fa09a22455b52b7ad549770
Adam Davies <acdavies001@gmail.com>
2022-02-09 18:31:45 -0600

    Group emotes with accordion menu

--- src/components/Overlay/Settings/Gallery.jsx
+++ src/components/Overlay/Settings/Gallery.jsx
@@ -1,16 +1,48 @@
-const EmoteGallery = ({ emotes, toggleEmote }) => (
-  <div id="gallery">
-    {emotes.map((emote, i) => (
-      <div
-        key={`${emote.name}_${i}`}
-        className={`emote${emote.selected ? ' selected' : ''}`}
-        onClick={() => toggleEmote(i)}
-      >
-        <img src={emote.url} className="h-auto m-auto" alt={emote.name} />
-        <p className="emote-name text-center mb-0">{emote.name}</p>
-      </div>
-    ))}
-  </div>
-);
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Accordion } from 'react-bootstrap';
+import GalleryGroup from './GalleryGroup';
+
+const EmoteGallery = ({ emotes, toggleEmote }) => {
+  const [groups, setGroups] = useState([]);
+
+  useEffect(() => {
+    let groups = [];
+    let newGroupStartIdx = 0;
+
+    for (let i = 0; i < emotes.length; i++) {
+      const prevEmote = i ? emotes[i - 1] : emotes[i];
+
+      if (
+        emotes[i].service !== prevEmote?.service ||
+        emotes[i].scope !== prevEmote?.scope ||
+        i === emotes.length - 1
+      ) {
+        groups.push({
+          service: prevEmote.service,
+          scope: prevEmote.scope,
+          emotes: emotes.slice(newGroupStartIdx, i),
+          idxOffset: newGroupStartIdx,
+        });
+
+        newGroupStartIdx = i;
+      }
+    }
+
+    setGroups(groups);
+  }, [emotes]);
+
+  return (
+    <Accordion id="gallery" defaultActiveKey={['twitch']} alwaysOpen>
+      {groups.map((group) => (
+        <GalleryGroup
+          key={`${group.service}_${group.scope}`}
+          toggleEmote={toggleEmote}
+          {...group}
+        />
+      ))}
+    </Accordion>
+  );
+};
 
 export default EmoteGallery;
---
+++ src/components/Overlay/Settings/GalleryGroup.jsx
@@ -0,0 +1,30 @@
+import { Accordion } from 'react-bootstrap';
+
+const properNames = {
+  twitch: 'Twitch',
+  bttv: 'BTTV',
+  ffz: 'FFZ',
+  '7tv': '7TV',
+  channel: 'Channel',
+  global: 'Global',
+};
+
+const GalleryGroup = ({ service, scope, emotes, idxOffset, toggleEmote }) => (
+  <Accordion.Item className="gallery-group" eventKey={service}>
+    <Accordion.Header className="group-title">{`${properNames[service]} (${properNames[scope]})`}</Accordion.Header>
+    <Accordion.Body className="emote-group">
+      {emotes.map((emote, i) => (
+        <div
+          key={emote.name}
+          className={`emote${emote.selected ? ' selected' : ''}`}
+          onClick={() => toggleEmote(i + idxOffset)}
+        >
+          <img src={emote.url} className="h-auto m-auto" alt={emote.name} />
+          <p className="emote-name text-center mb-0">{emote.name}</p>
+        </div>
+      ))}
+    </Accordion.Body>
+  </Accordion.Item>
+);
+
+export default GalleryGroup;
