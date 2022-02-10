5046651ca788d5fc8c767badf1e62a612823eea0
Adam Davies <acdavies001@gmail.com>
2022-02-09 20:31:23 -0600

    Add drop shadow to loading status

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,6 @@
 const Loading = ({ status }) => (
-  <div
-    id="loading"
-    className="d-flex flex-column justify-content-center align-items-center vw-100 vh-100 m-auto"
-  >
-    <h1 className="mb-2">{status.toUpperCase()}</h1>
+  <div className="d-flex flex-column justify-content-center align-items-center vw-100 vh-100 m-auto">
+    <h1 className="drop-shadow">{status.toUpperCase()}</h1>
   </div>
 );
 
