c6be476356886f3ab576ed43d7fc1151cd640d39
Adam Davies <acdavies001@gmail.com>
2022-02-28 16:59:16 -0600

    Abstract general settings

--- src/components/Overlay/Settings/Settings.jsx
+++ src/components/Overlay/Settings/Settings.jsx
@@ -1,33 +1,11 @@
-import { Modal, Button, Form } from 'react-bootstrap';
+import { Modal, Button } from 'react-bootstrap';
 
 import GalleryMenu from './GalleryMenu';
 import Gallery from './Gallery';
-
-const SPEED_MIN = 5;
-const SPEED_MAX = 200;
-
-const SIZE_MIN = 20;
-const SIZE_MAX = 300;
-
-const scaleVal = (number, inMin, inMax, outMin, outMax) =>
-  ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
-const modifySpeedIn = (val) => scaleVal(val, SPEED_MIN, SPEED_MAX, 0, 100);
-const modifySizeIn = (val) => scaleVal(val, SIZE_MIN, SIZE_MAX, 0, 100);
+import GeneralSettings from './General/GeneralSettings';
 
 const Settings = ({ data, update, visible, show }) => {
   const allowSave = data.emotes.filter((emote) => emote.selected).length > 0;
-
-  const modifySpeedOut = (val) =>
-    update({
-      type: 'setEmoteSpeed',
-      value: scaleVal(val, 0, 100, SPEED_MIN, SPEED_MAX),
-    });
-
-  const modifySizeOut = (val) =>
-    update({
-      type: 'setEmoteSize',
-      value: scaleVal(val, 0, 100, SIZE_MIN, SIZE_MAX),
-    });
 
   const showHelp = () => {
     show(false);
@@ -53,37 +31,7 @@ const Settings = ({ data, update, visible, show }) => {
         <Modal.Title>Settings</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <div className="row">
-          <div className="col border-end">
-            <Form.Label>Emote size</Form.Label>
-            <Form.Range
-              value={modifySizeIn(data.emoteSize)}
-              onChange={(e) => modifySizeOut(e.target.value)}
-            />
-            <Form.Label>Emote speed</Form.Label>
-            <Form.Range
-              value={modifySpeedIn(data.emoteSpeed)}
-              onChange={(e) => modifySpeedOut(e.target.value)}
-            />
-          </div>
-          <div className="col">
-            <Form.Label>Text size</Form.Label>
-            <Form.Range
-              value={data.textSize}
-              onChange={(e) =>
-                update({ type: 'setTextSize', value: e.target.value })
-              }
-            />
-            <Form.Check
-              type="switch"
-              label="Enable newly added emotes by default"
-              checked={data.emoteDefault}
-              onChange={(e) =>
-                update({ type: 'setEmoteDefault', value: e.target.checked })
-              }
-            />
-          </div>
-        </div>
+        <GeneralSettings data={data} update={update} />
         <h4 className="mt-4 border-bottom">Emotes</h4>
         <p className="text-body">
           These are all the Twitch sub, BTTV, FFZ, and 7TV emotes you have.
