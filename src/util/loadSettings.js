baf1bacb65c73415d96989ac921f8bc776f8966f
Adam Davies <acdavies001@gmail.com>
2022-03-15 19:08:44 -0500

    Fix URL param handling

--- src/util/loadSettings.js
+++ src/util/loadSettings.js
@@ -9,22 +9,24 @@ const defaults = {
 };
 
 function loadSettings(requestedEmotes, channel) {
+  const params = getParams();
   const currentSettings = JSON.parse(localStorage.getItem('settings'));
   let newSettings = {};
 
   if (!currentSettings || channel.toLowerCase() !== currentSettings.channel) {
     newSettings = Object.assign({}, defaults, {
       channel: channel.toLowerCase(),
-      emotes: requestedEmotes,
+      emotes: requestedEmotes.map((emote) => ({
+        ...emote,
+        selected: emoteParamVal(params, emote) ?? emote.selected,
+      })),
     });
   } else {
-    const params = getParams();
     const updatedEmotes = requestedEmotes.map((emote) => ({
       ...emote,
       selected:
-        emoteParamSelected(params, emote) ||
-        savedEmoteSelected(currentSettings, emote) ||
-        currentSettings.emoteDefault,
+        emoteParamVal(params, emote) ??
+        savedEmoteSelected(currentSettings, emote),
     }));
 
     newSettings = Object.assign({}, defaults, currentSettings, {
@@ -42,15 +44,17 @@ function savedEmoteSelected(currentSettings, emote) {
   )?.selected;
 }
 
-function emoteParamSelected(params, emote) {
+function emoteParamVal(params, emote) {
   if (emote.zeroWidth && params.zw !== undefined) {
     return params.zw;
+  } else if (params[emote.service][emote.scope] !== undefined) {
+    return params[emote.service][emote.scope];
   } else if (emote.scope === 'channel' && params.channel !== undefined) {
     return params.channel;
   } else if (emote.scope === 'global' && params.global !== undefined) {
     return params.global;
   } else {
-    return params[emote.service]?.[emote.scope] || false;
+    return undefined;
   }
 }
 
@@ -58,29 +62,34 @@ function getParams() {
   const params = new URLSearchParams(window.location.search);
 
   return {
-    zw: stringToBoolean(params.get('zw')),
-    channel: stringToBoolean(params.get('channel')),
-    global: stringToBoolean(params.get('global')),
+    zw: getParam(params, 'zw'),
+    channel: getParam(params, 'channel'),
+    global: getParam(params, 'global'),
     twitch: {
-      channel: stringToBoolean(params.get('twitch')),
+      channel: getParam(params, 'twitch'),
     },
     bttv: {
-      channel: stringToBoolean(params.get('bttv')),
-      global: stringToBoolean(params.get('bttvg')),
+      channel: getParam(params, 'bttv'),
+      global: getParam(params, 'bttvg'),
     },
     ffz: {
-      channel: stringToBoolean(params.get('ffz')),
-      global: stringToBoolean(params.get('ffzg')),
+      channel: getParam(params, 'ffz'),
+      global: getParam(params, 'ffzg'),
     },
     '7tv': {
-      channel: stringToBoolean(params.get('7tv')),
-      global: stringToBoolean(params.get('7tvg')),
+      channel: getParam(params, '7tv'),
+      global: getParam(params, '7tvg'),
     },
   };
 }
 
-function stringToBoolean(str) {
-  return str === 'true' ? true : str === 'false' ? false : undefined;
+function getParam(params, param) {
+  if (params.has(param)) {
+    const val = params.get(param);
+    return val === 'true' || val === '1' ? true : false;
+  } else {
+    return undefined;
+  }
 }
 
 export default loadSettings;
