69c4738c3461d4c3e012960840dfc3d05247860c
Adam Davies <acdavies001@gmail.com>
2023-07-02 17:15:54 -0500

    Switch back to existing Sass

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+};
 
 module.exports = nextConfig;
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {},
-  plugins: [],
-};
