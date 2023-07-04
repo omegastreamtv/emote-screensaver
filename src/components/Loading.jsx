3efaea8aa6094542b106ff798db383b0b9f9c259
Adam Davies <acdavies001@gmail.com>
2023-07-04 18:57:29 -0500

    Move Loading to TS

--- src/components/Loading.jsx
+++
@@ -1,7 +0,0 @@
-const Loading = ({ status }) => (
-  <div className="d-flex flex-column justify-content-center align-items-center vw-100 vh-100 m-auto">
-    <h1 className="drop-shadow">{status.toUpperCase()}</h1>
-  </div>
-);
-
-export default Loading;
---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+function Loading() {
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center vw-100 vh-100 m-auto">
+      <h1 className="drop-shadow">Loading emotes</h1>
+    </div>
+  );
+}
+
+export default Loading;
