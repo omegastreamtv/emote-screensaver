d563a4e62e53e8352d5827bbb2571cc77f05b414
Adam Davies <acdavies001@gmail.com>
2023-07-05 15:41:14 -0500

    Fix return type

--- src/util/emotes/index.ts
+++ src/util/emotes/index.ts
@@ -4,6 +4,13 @@ import * as bttv from './bttv';
 import * as ffz from './ffz';
 import * as stv from './7tv';
 
+// Use type predicate as type guard for result filter
+function fulfilledResult<T>(
+  res: PromiseSettledResult<T>
+): res is PromiseFulfilledResult<T> {
+  return res.status === 'fulfilled';
+}
+
 export default function getEmotes(channelId: string) {
   return Promise.allSettled([
     twitch.getChannelEmotes(channelId),
@@ -15,8 +22,8 @@ export default function getEmotes(channelId: string) {
     stv.getGlobalEmotes(),
   ]).then((resAll) =>
     resAll
-      .filter((res) => res.status === 'fulfilled')
-      .map((res) => res.status === 'fulfilled' && res.value)
+      .filter(fulfilledResult)
+      .map((res) => res.value)
       .flat()
   );
 }
