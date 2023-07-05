b841168f3e4963b7804f90af859ea4eed1ef7b05
Adam Davies <acdavies001@gmail.com>
2023-07-05 17:59:26 -0500

    Restructure overlay loading

--- src/app/(overlay)/channel/[channelName]/page.tsx
+++ src/app/(overlay)/channel/[channelName]/page.tsx
@@ -1,5 +1,18 @@
-function Overlay() {
-  return <h1>Test</h1>;
+import Overlay from '@/components/overlay/Overlay';
+import { getTwitchId } from '@/util/channel';
+import getEmotes from '@/util/emotes';
+
+type Props = {
+  searchParams?: Record<string, string>;
+};
+
+async function OverlayPage({ searchParams }: Props) {
+  if (searchParams?.channelName) {
+    const channelId = await getTwitchId(searchParams.channelName);
+    const emotes = await getEmotes(channelId as string);
+
+    return <Overlay channelName={searchParams.channelName} emotes={emotes} />;
+  }
 }
 
-export default Overlay;
+export default OverlayPage;
