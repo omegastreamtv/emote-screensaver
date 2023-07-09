ae9162c5e8719501d9a57b4e47b0456a4df754aa
Adam Davies <acdavies001@gmail.com>
2023-07-09 11:44:18 -0500

    Fix og/twitter image paths

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: isProd ? 'https://yeahbutdvds.com' : undefined,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-  },
-  experimental: {
-    appDir: true,
   },
 };
 
