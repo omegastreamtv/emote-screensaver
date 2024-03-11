b58ae066b15a4d9a242f9bdccb88e6f3abde70bf
Adam Davies <acdavies001@gmail.com>
2024-03-11 17:29:13 -0500

    Fix Twitch ID lookup

--- src/util/channel.ts
+++ src/util/channel.ts
@@ -1,21 +1,7 @@
-type UserData = {
-  data: {
-    username: string;
-    id: string;
-  };
-};
-
-type UserError = {
-  err: string;
-};
-
-type UserResponse = UserData | UserError;
-
 export const getTwitchId = async (channelName: string) => {
-  const res = await fetch(`http://itsathirdpartything.com/twitch/id/${channelName}`);
+  const res = await fetch(`https://itsathirdpartything.com/twitch/id/${channelName}`);
 
   if (res.ok) {
-    const data: UserResponse = await res.json();
-    return data;
+    return await res.text();
   }
 };
