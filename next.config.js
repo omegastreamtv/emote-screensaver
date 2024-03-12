7bf51c501319dee8e9b6bdb0df76bb56b7ac9b9e
Adam Davies <acdavies001@gmail.com>
2024-03-12 14:24:34 -0500

    Enable static export

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 
-const isProd = process.env.NODE_ENV === 'production';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: isProd ? 'https://yeahbutdvds.com' : undefined,
+  output: 'export',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
