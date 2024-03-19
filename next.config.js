66771b8effa882d42be8c4ffc1623db1e067a382
Adam Davies <acdavies001@gmail.com>
2024-03-18 21:38:07 -0500

    Remove basePath to fit gh-pages structure

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 
-const isProd = process.env.NODE_ENV === 'production';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/YEAHBUTDVDs' : '',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
