2ac4edaa9f2c261a5a01bc0ae1fc13fe434bbee7
Adam Davies <acdavies001@gmail.com>
2024-09-02 16:44:46 -0500

    Only use SSG in production

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: IS_PRODUCTION ? 'export' : undefined,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
